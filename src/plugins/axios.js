8e88136c6057b40707e210fd6b0b308f8fb10fa0
tanzil <tanjilislam90@gmail.com>
2024-02-01 23:34:48 +0600

    header added

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useAppStore } from '@/store/app'
 
 axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL
+axios.defaults.withCredentials = true
 
 axios.interceptors.request.use(
   (config) => {
