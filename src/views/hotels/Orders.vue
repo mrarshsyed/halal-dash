ec5ac1150efc440be373faa9e77a080c6449f859
tanjilislam90@gmail.com <tanjilislam90@gmail.com>
2025-02-14 22:06:50 +0600

    update order

--- src/views/hotels/Orders.vue
+++ src/views/hotels/Orders.vue
@@ -1,7 +1,176 @@
 <template>
   <div>
-    <!-- <FormComponent /> -->
+    <div v-if="orderDetails">
+      <v-card-text>
+        <v-btn
+          class="mb-4"
+          size="x-small"
+          color="primary"
+          icon="mdi-arrow-left"
+          @click="orderDetails = null"
+        />
+        <v-row>
+          <v-col cols="12" md="6">
+            <p class="text-h6 font-weight-bold">User Details</p>
+            <p class="font-weight-bold">User Name</p>
+            <p class="mb-2">
+              {{ orderDetails?.user?.name }}
+            </p>
+            <p class="font-weight-bold">User Email</p>
+            <p class="mb-2">
+              {{ orderDetails?.user?.email }}
+            </p>
+            <p class="font-weight-bold">User Phone</p>
+            <p>{{ orderDetails?.user?.contactNumber }}</p>
+          </v-col>
+
+          <v-col cols="12" md="6">
+            <p class="text-h6 font-weight-bold">Contact Details</p>
+            <p class="font-weight-bold">Contact Name</p>
+            <p class="mb-2">
+              {{
+                orderDetails?.emergingOrderBookingFinishPayload?.supplier_data
+                  ?.title
+              }}
+              {{
+                orderDetails?.emergingOrderBookingFinishPayload?.supplier_data
+                  ?.first_name_original
+              }}
+              {{
+                orderDetails?.emergingOrderBookingFinishPayload?.supplier_data
+                  ?.last_name_original
+              }}
+            </p>
+            <p class="font-weight-bold">Contact Email</p>
+            <p class="mb-2">
+              {{
+                orderDetails?.emergingOrderBookingFinishPayload?.supplier_data
+                  ?.email
+              }}
+            </p>
+            <p class="font-weight-bold">Contact Phone</p>
+            <p>
+              {{
+                orderDetails?.emergingOrderBookingFinishPayload?.supplier_data
+                  ?.phone
+              }}
+            </p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Booking Date</p>
+            <p>{{ formateDate(orderDetails?.createdAt) }}</p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">HEx Booking ID</p>
+            <p>{{ orderDetails?.bookingId }}</p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Supplier Booking ID</p>
+            <p>
+              {{
+                orderDetails?.emergingOrderBookingFormResponse?.data?.order_id
+              }}
+            </p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Hotel Name</p>
+            <p>{{ orderDetails?.hotelId?.name }}</p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">No of Room</p>
+            <p>
+              {{
+                orderDetails?.emergingOrderBookingFormPayload?.guests?.length
+              }}
+            </p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Number of Guests</p>
+            <p>
+              {{
+                getGuestCount(
+                  orderDetails?.emergingOrderBookingFinishPayload?.rooms,
+                  'adults'
+                )
+              }}
+              Adults,
+              {{
+                getGuestCount(
+                  orderDetails?.emergingOrderBookingFinishPayload?.rooms,
+                  'adults'
+                )
+              }}
+              Children
+            </p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Lead Guest Name</p>
+            {{
+              orderDetails?.emergingOrderBookingFinishPayload?.supplier_data
+                ?.title
+            }}
+            {{
+              orderDetails?.emergingOrderBookingFinishPayload?.supplier_data
+                ?.first_name_original
+            }}
+            {{
+              orderDetails?.emergingOrderBookingFinishPayload?.supplier_data
+                ?.last_name_original
+            }}
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Room Type</p>
+            <p>
+              {{ orderDetails?.rateHash?.room_data_trans?.main_name }}
+            </p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Check-in</p>
+            <p>
+              {{
+                formateDate(
+                  orderDetails?.emergingOrderBookingFormPayload?.checkin
+                )
+              }}
+            </p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Check-out Date</p>
+            <p>
+              {{
+                formateDate(
+                  orderDetails?.emergingOrderBookingFormPayload?.checkout
+                )
+              }}
+            </p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Preference</p>
+            <p>
+              {{ orderDetails?.customData?.note }}
+            </p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Payment Status</p>
+            <p>{{ orderDetails?.paymentStatus }}</p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Booking Done</p>
+            {{ orderDetails?.emergingBookingDone ? 'Yes' : 'No' }}
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Booking Confirmed</p>
+            {{ orderDetails?.emergingBookingConfirmed ? 'Yes' : 'No' }}
+          </v-col>
+          <v-col cols="12" md="6">
+            <p class="font-weight-bold">Price</p>
+            <p>AED {{ orderDetails?.price }}</p>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </div>
     <v-data-table-server
+      v-else
       class="mt-4"
       v-model:items-per-page="table_data.itemsPerPage"
       :headers="table_data.headers"
@@ -13,6 +182,15 @@
       @update:options="loadItems"
       :show-current-page="true"
     >
+      <template #item.action="{ item }">
+        <v-btn
+          icon="mdi-eye"
+          size="x-small"
+          variant="text"
+          color="primary"
+          @click="onDetails(item)"
+        />
+      </template>
       <template #item.bookingDate="{ item }">
         <p>
           {{ formateDate(item?.createdAt) }}
@@ -74,6 +252,7 @@ const table_data = ref({
   page: 1,
   serverItems: [],
   headers: [
+    { title: 'Action', key: 'action', align: 'start' },
     { title: 'Booking ID', key: 'bookingId', align: 'start' },
     { title: 'Booking Date', key: 'bookingDate', align: 'start' },
     { title: 'Hotel Name', key: 'hotel_name', align: 'start' },
@@ -106,6 +285,25 @@ const table_data = ref({
     { value: 40, title: '40' }
   ]
 })
+const orderDetails = ref(null)
+const onDetails = (data) => {
+  orderDetails.value = data
+  console.log(orderDetails.value)
+}
+const getGuestCount = (data, guestType) => {
+  if (!data || !Array.isArray(data)) return 0
+
+  return data.reduce((count, room) => {
+    if (!room.guests || !Array.isArray(room.guests)) return count
+
+    return (
+      count +
+      room.guests.filter((guest) =>
+        guestType === 'adults' ? !guest.is_child : guest.is_child
+      ).length
+    )
+  }, 0)
+}
 
 const loadItems = async ({ page, itemsPerPage }) => {
   table_data.value.page = page
