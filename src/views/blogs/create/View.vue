04a91f066cc8afb57f84cd48ad3fcbc8b3ce41d7
santokhan <santokhan1999@gmail.com>
2025-05-19 20:13:41 +0600

    Add permissions and blog pages

---
+++ src/views/blogs/create/View.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>Create New Blog</h2>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
---
+++ src/views/blogs/edit/View.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>Edit New Blog</h2>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
